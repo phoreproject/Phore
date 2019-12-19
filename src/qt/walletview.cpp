38187ec472e64604543d651cdd887b870a0e4cb9
Kolby Moroz <mrmetech@gmail.com>
2019-12-18 17:37:21 -0700

    Update walletview.cpp

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -205,7 +205,7 @@ void WalletView::setWalletModel(WalletModel* walletModel)
         masternodeListPage->setWalletModel(walletModel);
     }
     privacyPage->setModel(walletModel);
-    proposalList->setWalletModel(walletModel);
+    proposalListPage->setWalletModel(walletModel);
     receiveCoinsPage->setModel(walletModel);
     sendCoinsPage->setModel(walletModel);
 
